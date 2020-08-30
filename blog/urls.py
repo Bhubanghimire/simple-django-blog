00b07bd2b0735b16f7ab95f7d427e3552c1d04af
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-08-30 14:18:10 +0545

    crud commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 # blog/urls.py
 from django.urls import path
-from .views import BlogListView,BlogDetailView
+from .views import BlogListView,BlogDetailView,BlogCreateView,BlogEditView,BlogDeltView
 
 urlpatterns = [
     path('post/<int:pk>/',BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
+    path('post/new',BlogCreateView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit', BlogEditView.as_view(),name='post_edit'),
+    path('post/<int:pk>/dlt',BlogDeltView.as_view(),name='post_delete')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import post
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 class BlogListView(ListView):
@@ -11,3 +14,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model=post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogEditView(UpdateView):
+    model = post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeltView(DeleteView):
+    model = post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
